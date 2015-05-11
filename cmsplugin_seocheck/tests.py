1acf0cd3c9ac1084476f0c11d221843f247ef929
Christoph Reimers <christoph@superservice-international.com>
2015-05-11 09:57:51 -0400

    added tests

---
+++ cmsplugin_seocheck/tests.py
@@ -0,0 +1,47 @@
+from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
+
+from cms.api import create_page, create_title
+from cms.middleware.toolbar import ToolbarMiddleware
+from cms.toolbar.toolbar import CMSToolbar
+from cms.toolbar.items import ModalItem
+
+from djangocms_helper.utils import create_user
+
+
+class SEOViewTest(TestCase):
+
+    def get_request(self):
+        page = create_page(u'page one', 'fullwidth.html', language='en')
+        page.publish('en')
+
+        path = page.get_absolute_url()
+
+        admin_user = create_user(
+            'admin',
+            'admin@admin.com',
+            'admin',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        request = RequestFactory().get(path)
+        request.current_page = page
+        request.user = admin_user
+        request.session = {}
+
+        mid = ToolbarMiddleware()
+        mid.process_request(request)
+        return request
+
+    def test_get(self):
+        response = self.client.get('/en/originalseourl/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_toolbar(self):
+        request = self.get_request()
+        toolbar = CMSToolbar(request)
+        toolbar.get_left_items()
+        seo_menu = toolbar.menus['seo_check']
+        self.assertEqual(len(seo_menu.items), 1)
+        self.assertEqual(len(seo_menu.find_items(ModalItem, url=reverse('cmsplugin_seocheck:check_modal'))), 1)
