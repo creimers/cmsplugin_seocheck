1dacb72269513474c4932139ec7d2dff2d30e3e7
Christoph Reimers <christoph@superservice-international.com>
2015-05-11 10:52:38 -0400

    fixed test.urls mistake

--- cmsplugin_seocheck/tests/urls.py
+++ cmsplugin_seocheck/tests/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import patterns, include, url
 
 urlpatterns = i18n_patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('djangocms_blog.urls', namespace='djangocms_blog')),
+    url(r'^originalseourl/', include('cmsplugin_seocheck.urls', namespace="cmsplugin_seocheck")),
     url(r'^', include('cms.urls')),
 )
