9851071816d7c856073252e336c91ac97c677b2e
Christoph Reimers <christoph@superservice-international.com>
2015-05-11 10:57:03 -0400

    add admin to tests.urls

--- cmsplugin_seocheck/tests/urls.py
+++ cmsplugin_seocheck/tests/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls import patterns, include, url
 
+from django.contrib import admin
+
 urlpatterns = i18n_patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^originalseourl/', include('cmsplugin_seocheck.urls', namespace="cmsplugin_seocheck")),
     url(r'^', include('cms.urls')),
 )
